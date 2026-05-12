36b2605bc3bc3a64a81d02797862e6fff6626934
NourhanR <nrribrahim@gmail.com>
2026-05-12 13:46:51 +0300

    init UserController

---
+++ src/main/java/com/example/uniDemo/controllers/UserController.java
@@ -0,0 +1,10 @@
+package com.example.uniDemo.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/")
+class UserController {
+
+}
