a9e0e809dfe2273465009b00c4983494d51550aa
NourhanR <nrribrahim@gmail.com>
2026-05-12 13:24:45 +0300

    init commit

---
+++ src/main/java/com/example/uniDemo/UniDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.uniDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/uniDemo/UniDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uniDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
