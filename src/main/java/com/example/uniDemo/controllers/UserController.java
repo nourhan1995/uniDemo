25242311bc1e501d45d6cd39d080ea3534c8e901
NourhanR <nrribrahim@gmail.com>
2026-05-13 12:52:40 +0300

    added get users method that returns a dummy list of users

--- src/main/java/com/example/uniDemo/controllers/UserController.java
+++ src/main/java/com/example/uniDemo/controllers/UserController.java
@@ -1,8 +1,11 @@
 package com.example.uniDemo.controllers;
 
+import com.example.uniDemo.models.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("users")
@@ -13,5 +16,9 @@ class UserController {
         return "Hello from user controller";
     }
 
+    @GetMapping
+    public List<User> getUsers(){
+        return List.of(new User());
+    }
 
 }
