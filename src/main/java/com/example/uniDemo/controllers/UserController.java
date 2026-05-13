70a303a992db1bccb2255d5135d5a5ca37459623
NourhanR <nrribrahim@gmail.com>
2026-05-13 10:59:14 +0300

    added testController method

--- src/main/java/com/example/uniDemo/controllers/UserController.java
+++ src/main/java/com/example/uniDemo/controllers/UserController.java
@@ -1,10 +1,17 @@
 package com.example.uniDemo.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 @RequestMapping("users")
 class UserController {
+
+    @GetMapping("/hello")
+    public String testController(){
+        return "Hello from user controller";
+    }
+
 
 }
