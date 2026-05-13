3c486b79b1d9e802fc465d4edcca984d1d2ad63b
NourhanR <nrribrahim@gmail.com>
2026-05-13 15:22:54 +0300

    updated the UserController to use the created UserService

--- src/main/java/com/example/uniDemo/controllers/UserController.java
+++ src/main/java/com/example/uniDemo/controllers/UserController.java
@@ -1,14 +1,22 @@
 package com.example.uniDemo.controllers;
 
 import com.example.uniDemo.models.User;
+import com.example.uniDemo.services.UserService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
 @RequestMapping("users")
 class UserController {
+
+    private final UserService userService;
+
+    UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/hello")
     public String testController(){
@@ -17,17 +25,20 @@ class UserController {
 
     @GetMapping
     public List<User> getUsers(){
-        return List.of(new User());
+//        return List.of(new User());
+        return userService.getUsers();
     }
 
     @GetMapping("/{id}")
-    public User getUser(@PathVariable UUID id){
-        return new User();
+    public Optional<User> getUser(@PathVariable String id){
+//        return new User();
+        return userService.getUserById(id);
     }
 
     @PostMapping
     public User createUser(@RequestBody User user){
-        return new User(user.getName(), user.getEmail(), user.getAge());
+//        return new User(user.getName(), user.getEmail(), user.getAge());
+        return userService.createUser(user);
     }
 
 }
